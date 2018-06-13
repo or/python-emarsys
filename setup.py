51e650be65370dd7ab1042137365b10b3e888d50
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2018-06-13 11:18:47 +0200

    Bump version to 0.4
    0.3 was already existing as tag

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ KEYWORDS = "emarsys api wrapper"
 REPO_URL = "https://github.com/machtfit/python-emarsys"
 
 setup(name="python-emarsys",
-      version='0.2',
+      version='0.4',
       description="""Emarsys REST API wrapper for Python.""",
       author='Eugene Wee',
       url=REPO_URL,
